7a834775f919760cd8de46a4e6e617cc7d65ec2c
Anzer Bakhsh <anzerbakhsh@gmail.com>
2024-12-18 17:36:06 +0500

    change color theme in admin layout

--- src/pages/Admin/Sidebar/index.jsx
+++ src/pages/Admin/Sidebar/index.jsx
@@ -7,33 +7,33 @@ const Sidebar = ({isOpen,toggleSidebar,links}) => {
     <aside
       className={`${
         isOpen ? "w-[20vw]" : "w-[4vw]"
-      } h-screen bg-gray-800 text-white flex flex-col fixed top-0 left-0 transition-all duration-300`}
+      } h-screen bg-blue-600 text-white flex flex-col fixed top-0 left-0 transition-all duration-300`}
     >
      
-      <div className="flex items-center justify-between p-4 border-b border-gray-700">
+      <div className="flex items-center justify-between p-4 border-b border-white">
         <span className={`text-xl font-bold transition-all duration-300 ${!isOpen && "hidden"}`}>
           Admin
         </span>
         <button
           onClick={toggleSidebar}
-          className="text-white hover:bg-gray-700 p-2 rounded-md"
+          className="text-white hover:bg-white hover:text-blue-600 p-2 rounded-md"
         >
           <FaBars />
         </button>
       </div>
 
-      {/* Navigation Links */}
+     
       <nav className="flex-1 p-4 space-y-2">
         {links.map((link, index) => (
           <Link
             key={index}
             to={link.to}
-            className="flex items-center space-x-4 px-4 py-2 rounded-md hover:bg-gray-700"
+            className="flex  items-center space-x-4 py-2 rounded-md hover:bg-white hover:text-blue-600"
           >
-            <span className="text-xl">{link.icon}</span>
+            <span className="text-xl ">{link.icon}</span>
             <span
               className={`transition-all duration-300 ${
-                !isOpen && "opacity-0 translate-x-[-20px] pointer-events-none"
+                !isOpen && "opacity-0 translate-x-[-20px] pointer-events-none "
               }`}
             >
               {link.label}
