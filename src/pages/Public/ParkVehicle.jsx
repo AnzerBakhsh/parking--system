8f2f4d1bd237fd88612566850044a4c07f2cc0ce
Anzer Bakhsh <anzerbakhsh@gmail.com>
2024-12-18 16:55:23 +0500

    change github config

--- src/pages/Public/ParkVehicle.jsx
+++ src/pages/Public/ParkVehicle.jsx
@@ -54,6 +54,7 @@ const ParkVehicle = () => {
   };
 
 
+  
   return (
     <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg">
       <h2 className="text-2xl font-semibold mb-4 text-black">Park a Vehicle</h2>
