68ae5bd40a65de068e00ceb617deaa808c120d7e
Anzer Bakhsh <anzerbakhsh@gmail.com>
2024-12-19 16:33:06 +0500

    fix navbar

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -146,8 +146,8 @@ const loginWithGoogle = (event) => {
           >
             Log In
           </button>
-          <button onClick={loginWithGoogle} className="mt-2 px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
-        <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
+          <button onClick={loginWithGoogle} className="mt-2  px-4 py-2 border flex  m-auto gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+        <img className="w-6 h-6 " src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
         <span>Login with Google</span>
     </button>
           {/* <button
